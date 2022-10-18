7721a9fe75c1b6b9c002c0afbfdfb8795934d306
bbasanta238 <bbasanta238@gmail.com>
2022-10-18 13:49:32 +0545

    development of minting method and testing it

---
+++ test/mintingMethod_test.js
@@ -0,0 +1,24 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFT main minting and URI test", () => {
+	beforeEach(async () => {
+		[add1, add2, add3] = await ethers.getSigners();
+		contract = await ethers.getContractFactory("NFTMain");
+		deployedContract = await contract.deploy();
+	});
+	it("should have mint token", async () => {
+        await deployedContract.connect(add1).safeMint(add1.address,"NFT_1")
+		expect (await deployedContract.connect(add1).ownerOf(1)).to.equal(add1.address)
+		
+		console.log(
+			"token in the addres should be 1000000",
+			await deployedContract.connect(add1).ownerOf(1)
+		);
+	});
+
+    it("should have give the uri of metadata",async()=>{
+        await deployedContract.connect(add2).safeMint(add2.address,"NFT_1")
+        expect(await deployedContract.connect(add1).tokenURI(1)).to.equal("NFT_1")
+    })
+});
